c2820051b97cff77130e3568ee97b56353a16ef9
Nikita <nikita.gubarev31@gmail.com>
2022-08-06 23:08:07 +0300

    I added detail information in messges

--- GubchikWeatherBot.py
+++ GubchikWeatherBot.py
@@ -252,30 +252,52 @@ def time_today():
 async def get_and_send_information_about_one_day(soup: BeautifulSoup, message: types.Message):
     block, title = get_block_and_title_from(soup)
 
-    description = block.find("h3").text.strip()
-
     if time_today():
+        rain = block.find("table", class_="today__atmosphere").find_all("tr")[0].find("td").text.strip()
         wind = block.find("table", class_="today__atmosphere").find_all("tr")[1].find("td").text.strip()
+        humidity = block.find("table", class_="today__atmosphere").find_all("tr")[4].find("td").text.strip()
     else:
         column3 = block.find("ul", class_="today-hourly-weather").find_all("li")[2]
+
+        rain = column3.find("span", class_="precipitation-chance").text.strip()
         wind = column3.find("span", class_="wind-direction").text.strip()
+        humidity = column3.find("span", class_="humidity").text.strip()
 
-    await message.answer(f"{title}:\n"
-                         f"{description} (Вітер: {wind})")
+    await message.answer(f"{title}:\n\n"
+                         f"Вітер: {wind}\n"
+                         f"Вологість: {humidity}\n"
+                         f"Імовірність опадів: {rain}")
 
-    columns = block.find("ul", class_="today-hourly-weather").find_all("li")
-    for column in columns:
-        name = column.find("span", class_="today-hourly-weather__name").text.strip()
-        temp = column.find("span", class_="today-hourly-weather__temp").text.strip()
+    column = block.find("ul", class_="today-hourly-weather").find_all("li")
 
-        await message.answer(f"{name}: {temp}")
-        sleep(random())
+    name1 = column[0].find("span", class_="today-hourly-weather__name").text.strip()
+    temp1 = column[0].find("span", class_="today-hourly-weather__temp").text.strip()
+    desc1 = column[0].find("i", class_="today-hourly-weather__icon").get("title").strip()
+
+    name2 = column[1].find("span", class_="today-hourly-weather__name").text.strip()
+    temp2 = column[1].find("span", class_="today-hourly-weather__temp").text.strip()
+    desc2 = column[1].find("i", class_="today-hourly-weather__icon").get("title").strip()
+
+    name3 = column[2].find("span", class_="today-hourly-weather__name").text.strip()
+    temp3 = column[2].find("span", class_="today-hourly-weather__temp").text.strip()
+    desc3 = column[2].find("i", class_="today-hourly-weather__icon").get("title").strip()
+
+    name4 = column[3].find("span", class_="today-hourly-weather__name").text.strip()
+    temp4 = column[3].find("span", class_="today-hourly-weather__temp").text.strip()
+    desc4 = column[3].find("i", class_="today-hourly-weather__icon").get("title").strip()
+
+    await message.answer(f"{name1}: {temp1}\n({desc1})\n\n"
+                         f"{name2}: {temp2}\n({desc2})\n\n"
+                         f"{name3}: {temp3}\n({desc3})\n\n"
+                         f"{name4}: {temp4}\n({desc4})\n\n")
 
 
 async def get_and_send_information_about_many_days(soup: BeautifulSoup, message):
     block, title = get_block_and_title_from(soup)
 
     await message.answer(f"{title}:")
+
+    all_details = block.find("div", class_="item-table").find_all("ul")
 
     block = block.find("div", class_="swiper-wrapper")
     all_days = block.find_all("div", class_="swiper-slide")
@@ -285,12 +307,20 @@ async def get_and_send_information_about_many_days(soup: BeautifulSoup, message)
         date = day.find("div", class_="thumbnail-item__subtitle").text.strip()
         temp = day.find("div", class_="temperature-min").text.strip()
 
+        wind = all_details[0].find_all("li")[count].text.strip()
+        humidity = all_details[1].find_all("li")[count].text.strip()
+        rain = all_details[3].find_all("li")[count].text.strip()
+
         block_with_details = soup.find("div", class_="swiper-gallery")
         description = block_with_details.find_all("div", class_="description")[count].text.strip()
         description = description.split(": ")[1]
 
-        await message.answer(f"{name} ({date}): {temp}; {description}")
-        sleep(random())
+        await message.answer(f"{name} ({date}): {temp}\n"
+                             f"{description}\n\n"
+                             f"Вітер: {wind}\n"
+                             f"Вологість: {rain}\n"
+                             f"Імовірність опадів: {humidity}\n")
+        sleep(0.5)
 
 
 @DP.message_handler(Text(equals="закінчити спілкування", ignore_case=True))
