3fd13f8785bcca75bf78f5e9015729d0918a8139
Nikita <nikita.gubarev31@gmail.com>
2022-08-16 20:33:45 +0300

    I fixed small bug with naming

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,3 +1,3 @@
 from . import menu
 from .mailing import turning_on, turning_off, mailing_action
-from .commands import start, help, weather, moon, end
+from .commands import start, help, weather, moon, goodbye
