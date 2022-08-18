2a20361ed20bf5c933bbac181c104864a846c4ce
Nikita <nikita.gubarev31@gmail.com>
2022-08-18 13:52:39 +0300

    I added handler for menu function

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -1,17 +1,22 @@
 from aiogram import types
+from aiogram.dispatcher.filters import Text
 
+from bot_info import DP
 from constants import MY_DB
 from keyboard import make_reply_keyboard_markup, make_button
 
 
+@DP.message_handler(Text("← повернутися у головне меню", ignore_case=True))
 async def menu(message: types.Message):
     chat_IDs = MY_DB.chat_IDs
-    mailing_action = "Вимкнути" if message.from_user.id in chat_IDs else "Увімкнути"
+    mailing_btn_text = \
+        "Управління розсилкою" if message.from_user.id in chat_IDs else "Увімкнути розсилку"
 
     markup = make_reply_keyboard_markup(width=2)
     markup.add(make_button("Переглянути прогноз погоди"),
-               make_button("Переглянути фазу місяця"))
-    markup.add(make_button(f"{mailing_action} розсилку"))
+            make_button("Переглянути фазу місяця"))
+    markup.add(make_button(mailing_btn_text))
     markup.add(make_button("Закінчити спілкування"))
 
-    await message.answer("Виберіть подальші дії", reply_markup=markup)
+    await message.answer("Ви в головному меню\n\n"
+                         "Виберіть подальші дії", reply_markup=markup)
