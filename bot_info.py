b3dc23345938d88ce1eece290f6cc6f801e17998
Nikita <nikita.gubarev31@gmail.com>
2022-08-23 11:10:06 +0300

    I made small refactoring

--- bot_info.py
+++ bot_info.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data.config import BOT_TOKEN
--- constants.py
+++ constants.py
@@ -3,6 +3,4 @@ import os
 from utils.db_api import DB
 
 MY_DB = DB()
-CHAT_IDs = []
-
 BASE_DIR = os.path.dirname(__file__)
