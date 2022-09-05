45219c11b97321002697335e62d51cf7c9294251
Nikita <nikita.gubarev31@gmail.com>
2022-09-05 14:59:51 +0300

    I added new files with localities data on ru and en languages

---
+++ data/localities/__init__.py
@@ -0,0 +1,8 @@
+from .uk_ukr_localities import UK_UKR_LOCALITIES
+from .uk_abroad_localities import UK_ABROAD_LOCALITIES
+
+from .en_urk_localities import EN_UKR_LOCALITIES
+from .en_abroad_localities import EN_ABROAD_LOCALITIES
+
+from .ru_ukr_localities import RU_UKR_LOCALITIES
+from .ru_abroad_localities import RU_ABROAD_LOCALITIES
