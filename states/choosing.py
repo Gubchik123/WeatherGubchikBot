d99915b4b080fcc9d76877d835e448d8b06cd761
Nikita <nikita.gubarev31@gmail.com>
2022-09-05 14:58:20 +0300

    I added new State row for choosing language

--- states/choosing.py
+++ states/choosing.py
@@ -2,6 +2,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class Choosing(StatesGroup):
+    language = State()
+
     region = State()
     region_title = State()
     period = State()
