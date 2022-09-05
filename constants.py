8a5069e1ebd85f4c8aa7d8d8df3636e25bb38edb
Nikita <nikita.gubarev31@gmail.com>
2022-09-05 14:56:50 +0300

    I added new constant for Language class

--- constants.py
+++ constants.py
@@ -2,7 +2,12 @@ import os
 
 from utils.db_api import DB
 from utils.class_SelectedInfo import SelectedInfo
+from utils.languages.class_Language import Language
 
 MY_DB = DB()
+
 INFO = SelectedInfo()
+
+TEXT = Language()
+
 BASE_DIR = os.path.dirname(__file__)
