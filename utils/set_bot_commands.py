6ba4133b7e4f10e3efbe916dcc58df0313ba9439
Nikita <nikita.gubarev31@gmail.com>
2022-09-05 15:01:41 +0300

    I changed the method of adding default bot commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,32 +1,35 @@
+from googletrans import Translator
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
 from bot_info import BOT
 
 
 async def set_default_commands(message):
-    bot_commands = {
-        "en": [
-            BotCommand("start",   "Start of working with bot"),
-            BotCommand("help",    "General rules for using"),
-            BotCommand("weather", "Showing information about the weather"),
-            BotCommand("moon",    "Showing information about moon phase"),
-            BotCommand("goodbye", "Finish of working with bot")
-        ],
-        "uk": [
-            BotCommand("start",   "Початок роботи з ботом"),
-            BotCommand("help",    "Основні правила використання"),
-            BotCommand("weather", "Відображення інформації про погоду"),
-            BotCommand("moon",    "Відображення інформації про фазу місяця"),
-            BotCommand("goodbye", "Кінець роботи з ботом")
-        ],
-        "ru": [
-            BotCommand("start",   "Начало работы с ботом"),
-            BotCommand("help",    "Основные правила использования"),
-            BotCommand("weather", "Отображение информации о погоде"),
-            BotCommand("moon",    "Отображение информации о фазе луны"),
-            BotCommand("goodbye", "Конец работы с ботом")
-        ],
+    translator = Translator()
+
+    uk_commands = {
+        "start":    "Початок роботи з ботом",
+        "language": "Змінення мови бота",
+        "help":     "Відображення основних правил використання",
+        "weather":  "Відображення інформації про погоду",
+        "moon":     "Відображення інформації про фазу місяця",
+        "goodbye":  "Завершення роботи з ботом"
     }
+
+    bot_commands: dict = {}
+
+    for lang_code in ("uk", "en", "ru"):
+        commands: list = []
+
+        for command, text in uk_commands.items():
+            if lang_code != "uk":
+                text = translator.translate(text, dest=lang_code).text
+
+            commands.append(
+                BotCommand(command, text)
+            )
+
+        bot_commands[lang_code] = commands
 
     for language_code, commands in bot_commands.items():
         await BOT.set_my_commands(
