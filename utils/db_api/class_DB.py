e0673ce5ae183f56c1400899487f2e46cb81740d
Nikita <nikita.gubarev31@gmail.com>
2022-09-06 18:49:34 +0300

    I added some code for working with language in db

--- utils/db_api/class_DB.py
+++ utils/db_api/class_DB.py
@@ -20,7 +20,9 @@ class DB:
         """Method for returning information for mailing from database"""
         with self._connection.cursor() as cursor:
             cursor.execute(
-                "SELECT chat_id, mute, city, time_title, time, type, time_int, city_title FROM mailing;")
+                """SELECT chat_id, mute, city, time_title, time, 
+                type, time_int, city_title, lang FROM mailing;"""
+            )
             data = cursor.fetchall()
         return data
 
@@ -33,11 +35,12 @@ class DB:
         with self._connection.cursor() as cursor:
             sql_adding_query = f"""
             INSERT INTO mailing
-            (chat_id, mute, name, city, time, time_title, type, time_int, city_title)
+            (chat_id, mute, name, city, time, time_title, 
+            type, time_int, city_title, lang)
             VALUES
             ({user.chat_id}, {user.selected_mute_mode}, '{user.name}', 
             '{info.city}', '{info.time}', '{info.time_title}', '{info.type}',
-            {user.selected_time}, '{info.city_title}');
+            {user.selected_time}, '{info.city_title}', '{info.lang}');
             """
             cursor.execute(sql_adding_query)
 
@@ -46,7 +49,8 @@ class DB:
             "mute": user.selected_mute_mode,
             "city": info.city_title,
             "time": info.time_title,
-            "time_int": user.selected_time
+            "time_int": user.selected_time,
+            "lang": info.lang
         }
 
     def update_user_with(self, chat_id: int, what_update: str, new_item):
@@ -79,9 +83,19 @@ class DB:
                 sql_update_query = f"""
                 UPDATE mailing SET time_int = {new_item} WHERE chat_id = {chat_id}
                 """
+            elif what_update == "lang":
+                sql_update_query = f"""
+                UPDATE mailing SET lang = '{new_item}' WHERE chat_id = {chat_id}
+                """
             cursor.execute(sql_update_query)
 
         self.__users_info[chat_id][what_update] = new_item
+
+    def update_user_lang_with_(self, chat_id: int, lang: str):
+        data: dict = self.get_information_about_user_with_(chat_id)
+        
+        if data["lang"] != lang:
+            self.update_user_with(chat_id, what_update="lang", new_item=lang)
 
     def delete_user_with(self, chat_id: int):
         """Method for deleting user from database"""
@@ -106,5 +120,6 @@ class DB:
             "mute": data[1],
             "city": data[7],
             "time": data[3],
-            "time_int": data[6]
+            "time_int": data[6],
+            "lang": data[8]
         } for data in users_info}
