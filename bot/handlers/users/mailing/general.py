55b51c046115b5e90895d4e5b16884aaabbf552e
Nikita <nikita.gubarev31@gmail.com>
2022-10-24 13:27:43 +0300

    I added new general function

--- bot/handlers/users/mailing/general.py
+++ bot/handlers/users/mailing/general.py
@@ -14,3 +14,11 @@ async def there_is_no_such_type_of_answer_try_again(func, message: types.Message
     global TEXT
     await message.answer(TEXT().there_are_not_such_type_of_answer_message())
     await func(message)
+
+
+def check_language_from_(text: str):
+    global TEXT
+    lang_code = "uk" if "увімкнути" in text else (
+        "ru" if "включить" in text else "en"
+    )
+    TEXT.check_language_by_(lang_code)
