e035dfacf2d587d75fd7acd9c26b1f7243e54857
Nikita <nikita.gubarev31@gmail.com>
2022-11-21 14:03:12 +0200

    Change db url for bot

--- bot/data/config.py
+++ bot/data/config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5628317296:AAG7s6L7r_eI6f_HvYh41IBjbcGs8b5i8v0"
 
-DB_URI = "postgres://lbxeasbbvuqadu:a63d5de1bb6de2fd6e7d05e48fb224ec7c1b0b7b1af48f62628ae46329e04121@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/d1tvfr7dmbdv8s"
+DB_URI = "postgres://xpaablic:cfYfOpHmypp4KFku_vU0PCkwGw8IPuUR@arjuna.db.elephantsql.com/xpaablic"
