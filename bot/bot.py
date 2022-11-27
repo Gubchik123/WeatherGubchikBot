2d0de9ad68930278f380a8762624fb4ca0a589ae
Nikita <nikita.gubarev31@gmail.com>
2022-11-27 12:10:24 +0200

    I created the function for setting mailing hours

--- bot/bot.py
+++ bot/bot.py
@@ -1,39 +1,34 @@
-import schedule
-from time import sleep
-from threading import Thread
+import logging
 from datetime import datetime
 
 from aiogram import executor
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import handlers
-from bot_info import DP
+from bot_info import DP, BOT
 from utils.set_bot_commands import set_default_commands
 from handlers.users.daily_mailing import send_to_users
 
 
-def get_needed_hours():
-    return (
-        ("04", "07", "10", "13", "16", "19")
-        if datetime.now().month in [1, 2, 3, 11, 12]
-        else ("03", "06", "09", "12", "15", "18")
-    )
-
-
-def check_schedule_time():
-    while True:
-        schedule.run_pending()
-        sleep(60)
+def set_hours_for_daily_mailing():
+    scheduler = AsyncIOScheduler(timezone="Europe/Kiev")
+    for hour in (6, 9, 12, 15, 18, 21):
+        scheduler.add_job(
+            send_to_users,
+            trigger="cron",
+            hour=hour,
+            minute=0,
+            start_date=datetime.now(),
+            kwargs={"bot": BOT},
+        )
+    scheduler.start()
 
 
 if __name__ == "__main__":
-    print(f"Hour in the server: {datetime.now().hour}")
+    logger = logging.getLogger()
+    logger.info(f"Hour in the server: {datetime.now().hour}")
 
-    # Loop for creating tasks at specials hours for daily mailing
-    for hour in get_needed_hours():
-        schedule.every().day.at(f"{hour}:00").do(send_to_users)
-
-    # Other thread for checking loop
-    Thread(target=check_schedule_time, daemon=True).start()
+    set_hours_for_daily_mailing()
 
     # Start bot polling and on start set default bot commands for 3 language
     executor.start_polling(DP, skip_updates=True, on_startup=set_default_commands)
