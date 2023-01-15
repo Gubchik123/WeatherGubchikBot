d8786da88a51f457cd9a21297e2107bb2ede886b
Nikita <nikita.gubarev31@gmail.com>
2023-01-15 12:26:13 +0200

    I made refactoring in the module for mailing

--- bot/handlers/users/daily_mailing.py
+++ bot/handlers/users/daily_mailing.py
@@ -1,5 +1,4 @@
 import logging
-import traceback
 from datetime import datetime
 
 from bot_info import BOT
@@ -12,44 +11,12 @@ from .weather.parsing import (
 )
 
 
-def get_users_with_mailing_on_current_time() -> tuple:
-    """For getting users with current time for mailing"""
-    datetime_now = datetime.now()
-
-    # Hour in Ukraine UTC +2 if it's Nov, Dec, Jan, Feb, Mar else +3
-    added_hour = 2 if datetime_now.month in [1, 2, 3, 11, 12] else 3
-    ukrainian_hour = datetime_now.hour + added_hour
-
-    return tuple(
-        user for user in MY_DB.get_all_users() if user.time_int == ukrainian_hour
-    )
-
-
-def fill_weather_information_by_(user: UserDBInfo) -> None:
-    """For filling info object with user data for weather searching"""
-    INFO.clean_information()
-
-    INFO.city = user.city
-    INFO.time = user.time
-    INFO.type = user.type
-
-
-def get_message_text_by_(lang_code: str) -> str:
-    """For getting message text with weather information"""
-    TEXT.change_on(lang_code)
-
-    return (
-        get_information_about_one_day()
-        if INFO.about_one_day
-        else get_information_about_many_days()
-    )
-
-
 async def send_to_users() -> None:
     """For sending weather message to users with current time for mailing"""
-    for user in get_users_with_mailing_on_current_time():
+    for user in _get_users_with_mailing_on_current_time():
         try:
-            fill_weather_information_by_(user)
+            TEXT.change_on(user.lang)
+            _fill_weather_information_by_(user)
 
             await BOT.send_message(
                 user.chat_id,
@@ -58,13 +25,42 @@ async def send_to_users() -> None:
             )
             await BOT.send_message(
                 user.chat_id,
-                get_message_text_by_(user.lang),
+                _get_weather_info_message(),
                 disable_notification=user.mute,
             )
         except Exception as e:
             logger = logging.getLogger()
             logger.error(f"Exception in daily mailing with user: {user.chat_id}")
             logger.error(str(e))
-            exception_place = traceback.format_exception(e)[-2].strip().replace("\n", " ")
-            logger.error(f'Place: {exception_place}')
             continue
+
+
+def _get_users_with_mailing_on_current_time() -> tuple:
+    """For getting users with current time for mailing"""
+    datetime_now = datetime.now()
+
+    # If it's Nov, Dec, Jan, Feb or Mar, the hour in Ukraine is UTC +2, otherwise +3
+    added_hour = 2 if datetime_now.month in [1, 2, 3, 11, 12] else 3
+    ukrainian_hour = datetime_now.hour + added_hour
+
+    return tuple(
+        user for user in MY_DB.get_all_users() if user.time_int == ukrainian_hour
+    )
+
+
+def _fill_weather_information_by_(user: UserDBInfo) -> None:
+    """For filling info object with user data for weather searching"""
+    INFO.clean_information()
+
+    INFO.city = user.city
+    INFO.time = user.time
+    INFO.type = user.type
+
+
+def _get_weather_info_message() -> str:
+    """For getting message text with weather information"""
+    return (
+        get_information_about_one_day()
+        if INFO.about_one_day
+        else get_information_about_many_days()
+    )
