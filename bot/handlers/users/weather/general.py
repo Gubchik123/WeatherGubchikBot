8b786f9c045dcc5ba933d8ac6bc98c1c55619c34
Nikita <nikita.gubarev31@gmail.com>
2023-06-01 21:42:46 +0300

    Add small workaround (for now) to fix 410 response status code

--- bot/handlers/users/weather/general.py
+++ bot/handlers/users/weather/general.py
@@ -8,7 +8,7 @@ from user_agent import generate_user_agent
 
 from ..menu import menu
 from bot_info import BOT
-from constants import TEXT
+from constants import INFO, TEXT
 
 
 logger = logging.getLogger("my_logger")
@@ -20,11 +20,17 @@ class InvalidResponse(Exception):
 
 def _get_response_from_(url: str, lang_code: str) -> requests.Response:
     """For sending GET request to url and getting response"""
-    response = requests.get(
-        url,
-        headers={"user-agent": generate_user_agent().strip()},
-        cookies={"cookie": f"needed_thing=''; default_lang={lang_code};"},
-    )
+    headers = {"user-agent": generate_user_agent().strip()}
+    cookies = {"cookie": f"needed_thing=''; default_lang={lang_code};"}
+
+    response = requests.get(url, headers=headers, cookies=cookies)
+
+    if response.status_code == 410:
+        INFO.type = "weather"
+        INFO.time = ""
+        response = requests.get(
+            INFO.generated_url, headers=headers, cookies=cookies
+        )
     if response.status_code != 200:
         raise InvalidResponse(
             f"InvalidResponse from the site ({response.status_code}); url={url}"
@@ -52,14 +58,16 @@ async def send_message_to_user_about_error(
     if message_to_user:
         await message.answer(TEXT().try_again_message())
         await menu(message)
-        
+
     error_message = f"Exception{error_place}: {error}"
 
     logger.error(error_message)
     await _send_message_about_error_to_me(message, error_message)
 
 
-async def _send_message_about_error_to_me(message: types.Message, error_message: str) -> str:
+async def _send_message_about_error_to_me(
+    message: types.Message, error_message: str
+) -> str:
     """For sending to me message about weather info user got"""
     my_chat_id = int(os.getenv("MY_TELEGRAM_CHAT_ID"))
 
