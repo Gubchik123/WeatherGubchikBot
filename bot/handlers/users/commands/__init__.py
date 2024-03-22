908fee7efd7253a3009bd28ed03c06d1fb581426
Nikita <nikita.gubarev31@gmail.com>
2024-03-22 12:45:55 +0200

    Add the help command handler

--- bot/handlers/users/commands/__init__.py
+++ bot/handlers/users/commands/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 
 from .start import router as start_command_router
+from .help import router as help_command_router
 
 
 commands_router = Router()
 
 
-commands_router.include_router(start_command_router)
+commands_router.include_routers(start_command_router, help_command_router)
--- bot/handlers/users/commands/help.py
+++ bot/handlers/users/commands/help.py
@@ -1,13 +1,41 @@
-from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandHelp
-
-from bot_info import DP
-from constants import TEXT
-from handlers.users.menu import menu
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.utils.i18n import gettext as _
 
 
-@DP.message_handler(CommandHelp())
-async def command_help(message: types.Message) -> None:
-    """The handler for the 'help' command for the general rules of using"""
-    await message.answer(TEXT().general_rules())
-    await menu(message)
+router = Router()
+
+
+@router.message(Command(commands=["help"]))
+async def handle_help_command(message: Message):
+    """Handles the /help command."""
+    await message.answer(
+        _(
+            """
+            Bot commands:
+            /start - Start working with the bot
+            /language - Change the language of the bot
+            /help - Display of basic usage rules
+            /weather - Display information about the weather
+            /goodbye - Ending work with the bot
+
+            Sign up for the newsletter to receive daily weather information in the city of your choice (you can turn it off at any time)
+
+            I advise you to use the buttons for the intended result
+
+            Enjoy using!!!
+
+            Bot author contacts:
+            CV site: https://nikita-hubariev.me
+            LinkedIn: https://www.linkedin.com/in/nikita-hubariev
+            Instagram: https://www.instagram.com/notwhale.1746
+
+            Other projects of the author are available on:
+            Project board: https://gubchik123-project-board.netlify.app
+            GitHub: https://github.com/Gubchik123
+            """.replace(
+                "            ", ""
+            )
+        )
+    )
