4a31abe78fd3bbeefce3126fc1a7fa59ab5e3d6d
Nikita <nikita.gubarev31@gmail.com>
2024-03-22 13:00:11 +0200

    Add the goodbye command handler

--- bot/handlers/users/commands/__init__.py
+++ bot/handlers/users/commands/__init__.py
@@ -2,9 +2,12 @@ from aiogram import Router
 
 from .start import router as start_command_router
 from .help import router as help_command_router
+from .goodbye import router as goodbye_command_router
 
 
 commands_router = Router()
 
 
-commands_router.include_routers(start_command_router, help_command_router)
+commands_router.include_routers(
+    start_command_router, help_command_router, goodbye_command_router
+)
--- bot/handlers/users/commands/goodbye.py
+++ bot/handlers/users/commands/goodbye.py
@@ -1,33 +1,24 @@
-from aiogram import types
-from aiogram.dispatcher.filters import Text
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.utils.i18n import gettext as _, lazy_gettext as __
 
-from bot_info import DP
-from constants import TEXT
-from keyboard import make_keyboard, make_button
-
-from ..menu import _check_language_from_
+from keyboards.default.maker import make_keyboard, make_button
 
 
-answer_options = ("закінчити спілкування", "закончить общение", "end communication")
+router = Router()
 
 
-@DP.message_handler(commands="goodbye")
-@DP.message_handler(Text(equals=answer_options[0], ignore_case=True))
-@DP.message_handler(Text(equals=answer_options[1], ignore_case=True))
-@DP.message_handler(Text(equals=answer_options[2], ignore_case=True))
-async def command_goodbye(message: types.Message) -> None:
-    """The handler for the 'goodbye' command"""
-    user_text = message.text.lower()
-
-    if user_text in answer_options:
-        _check_language_from_(user_text, uk_word="спілкування", ru_word="общение")
-
-    markup = make_keyboard(width=1)
-    markup.add(make_button("/start"))
-
+@router.message(Command(commands=["goodbye"]))
+@router.message(F.text.lower() == __("end communication"))
+async def handle_goodbye_command(message: Message):
+    """Handles the /goodbye command."""
     await message.answer_sticker(
         "CAACAgIAAxkBAAICBGLIifDJ3jPz291sEcRKE5EO4j99AALsAwAC0lqIAZ0zny94Yp4oKQQ"
     )
     await message.answer(
-        TEXT().goodbye_message(message.from_user.first_name), reply_markup=markup
+        _(
+            "Bye, {name}, come bake again!\nNext time just type or press /start"
+        ).format(name=message.from_user.first_name),
+        reply_markup=make_keyboard([[make_button("/start")]], one_time=True),
     )
