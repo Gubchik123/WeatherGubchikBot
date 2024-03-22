718f724fcc7323ffe7dfd6e990035e2692fc1247
Nikita <nikita.gubarev31@gmail.com>
2024-03-22 12:39:40 +0200

    Refactor the language middleware to handle changing lang event

--- bot/middlewares/language.py
+++ bot/middlewares/language.py
@@ -1,8 +1,7 @@
-from typing import Dict, Any, Optional
-
-from aiogram.types import TelegramObject, User as TelegramUser
+from typing import Union, Dict, Any, Optional
 
 from aiogram.utils.i18n.middleware import SimpleI18nMiddleware
+from aiogram.types import Message, CallbackQuery, User as TelegramUser
 
 from utils.db.crud.user import get_user_locale_by_
 
@@ -11,9 +10,17 @@ class LanguageMiddleware(SimpleI18nMiddleware):
     """Middleware for getting user language."""
 
     async def get_locale(
-        self, event: TelegramObject, data: Dict[str, Any]
+        self, event: Union[Message, CallbackQuery], data: Dict[str, Any]
     ) -> str:
         """Returns user language code."""
+        try:
+            if event.data.startswith(
+                "btn_start_lang"
+            ) or event.data.startswith("btn_menu_lang"):
+                return event.data.split("_")[-1]
+        except AttributeError:
+            pass  # Event is Message and doesn't have 'data'
+
         telegram_user_locale = await super().get_locale(event, data)
         telegram_user: Optional[TelegramUser] = data.get(
             "event_from_user", None
