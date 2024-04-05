3212b79da9105180d01f5195e9110a85a914bdf6
Nikita <nikita.gubarev31@gmail.com>
2024-04-05 16:44:44 +0300

    Extract new functions to get error message from the send_message_about_error

--- bot/utils/error.py
+++ bot/utils/error.py
@@ -14,39 +14,44 @@ async def send_message_about_error(
     message_to_user: bool = True,
 ) -> None:
     """Sends the given error message to the admins (and user) and logs it."""
-    if message_to_user:
-        await _send_message_to_user_about_error(event)
+    logging.error(f"{error.capitalize()}{error_place}")
+    await send_to_admins(_get_admin_error_message(event, error, error_place))
 
+    if message_to_user:
+        await event.answer(_get_user_error_message())
+
+
+def _get_admin_error_message(
+    event: Union[Message, CallbackQuery], error: str, error_place: str
+) -> str:
+    """Returns the default admin error message."""
     user = event.from_user
     reason = (
         f"message: '{event.text}'"
         if isinstance(event, Message)
         else f"clicking '{event.data}' on message\n---\n{event.message.text}"
     )
-    error_message = (
-        f"<b>Exception{error_place}</b> "
+    return (
+        f"❗️ <b>Exception{error_place}</b> "
         f"with user <code>{user.id}</code> (@{user.username})\n\n"
         f"{error.capitalize()}\n\nafter {reason}"
     )
-    logging.error(f"{error.capitalize()}{error_place}")
-    await send_to_admins(error_message)
 
 
-async def _send_message_to_user_about_error(
-    event: Union[Message, CallbackQuery]
-):
-    """Sends the default message to user about error."""
+def _get_user_error_message() -> str:
+    """Returns the default user error message."""
     try:
-        message_text = _(
-            "An error occurred! :(\n"
+        return _(
+            "An error occurred! 😥\n\n"
+            "The admins have already been notified about this "
+            "and will fix the problem as soon as possible.\n\n"
             "Please, try again or restart the bot with the /start command."
         )
     except LookupError:
-        logging.warning(
-            "LookupError in the send_message_to_user_about_error function!"
-        )
-        message_text = (
-            "An error occurred! :(\n"
+        logging.warning("LookupError in the get_error_message function!")
+        return (
+            "An error occurred! 😥\n\n"
+            "The admins have already been notified about this "
+            "and will fix the problem as soon as possible.\n\n"
             "Please, try again or restart the bot with the /start command."
         )
-    await event.answer(message_text)
