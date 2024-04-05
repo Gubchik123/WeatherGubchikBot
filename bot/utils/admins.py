aea545349f27e88f530d861141c2f3699ced9386
Nikita <nikita.gubarev31@gmail.com>
2024-04-05 17:00:07 +0300

    Add support for sending documents to admins

--- bot/utils/admins.py
+++ bot/utils/admins.py
@@ -2,6 +2,7 @@ import logging
 from typing import Optional
 
 from aiogram import Bot
+from aiogram.types import FSInputFile
 from aiogram.client.default import DefaultBotProperties
 
 from data.config import ADMINS, BOT_TOKEN
@@ -16,7 +17,7 @@ async def notify_admins_on_startup_of_(bot: Bot) -> None:
             logging.exception(err)
 
 
-async def send_to_admins(message: str, temp_bot: Optional[Bot] = None):
+async def send_to_admins(*messages: str, temp_bot: Optional[Bot] = None):
     """Sends the given message to all admins"""
     is_temp_bot_none = temp_bot is None
 
@@ -25,8 +26,14 @@ async def send_to_admins(message: str, temp_bot: Optional[Bot] = None):
             token=BOT_TOKEN,
             default=DefaultBotProperties(parse_mode="HTML"),
         )
-    for admin in ADMINS:
-        await temp_bot.send_message(chat_id=admin, text=message)
+    for admin_chat_id in ADMINS:
+        for message in messages:
+            if message.endswith(".txt"):
+                await temp_bot.send_document(
+                    admin_chat_id, document=FSInputFile(message)
+                )
+            else:
+                await temp_bot.send_message(admin_chat_id, text=message)
 
     if is_temp_bot_none:
         await temp_bot.session.close()
