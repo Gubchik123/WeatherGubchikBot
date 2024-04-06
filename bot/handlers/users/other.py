3085d8724ecd0d31c597385086c614102df83417
Nikita <nikita.gubarev31@gmail.com>
2024-04-06 13:32:28 +0300

    Handle TelegramBadRequest exception in the handle_all_other_messages function

--- bot/handlers/users/other.py
+++ bot/handlers/users/other.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.utils.i18n import gettext as _
+from aiogram.exceptions import TelegramBadRequest
 
 
 router = Router()
@@ -9,9 +10,12 @@ router = Router()
 @router.message()
 async def handle_all_other_messages(message: Message):
     """Handles all other messages."""
-    await message.answer(
-        _(
-            "I don't understand you :(\n"
-            "I advise you to use the buttons or commands for the intended result."
+    try:
+        await message.answer(
+            _(
+                "I don't understand you :(\n"
+                "I advise you to use the buttons or commands for the intended result."
+            )
         )
-    )
+    except TelegramBadRequest:
+        pass
