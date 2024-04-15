687abcf08eb498951ebec4aa974f00b0f48456ff
Nikita <nikita.gubarev31@gmail.com>
2024-04-15 19:49:26 +0300

    Refactor city searching logic for the 'meteoprog' weather provider

--- bot/utils/weather/meteoprog/search.py
+++ bot/utils/weather/meteoprog/search.py
@@ -1,6 +1,6 @@
 import aiohttp
 from collections import OrderedDict
-from typing import NamedTuple, Tuple, Union, List
+from typing import NamedTuple, Tuple, Union
 
 from emoji import emojize
 from user_agent import generate_user_agent
@@ -25,11 +25,18 @@ async def get_searched_data_with_(
 ) -> Tuple[Union[str, dict], bool]:
     """Returns searched dict of countries or cities, or exact city."""
     response_json = await _get_response_json_by_(user_input, lang_code)
-    cities = [City(*data) for data in response_json["data"]]
+    cities = OrderedDict()
 
-    if _is_match_100_in_(cities, user_input):
-        return cities[0].en_name, True
-    return _get_filtered_(cities, lang_code), False
+    for city in [City(*data) for data in response_json["data"]]:
+        if _is_terrorist_(city):
+            continue
+        if city.name.lower() == user_input:
+            return city.en_name, True
+        country_flag_emoji = _get_country_flag_emoji_by_(
+            city.country, lang_code
+        )
+        cities[f"{country_flag_emoji} {city.name}"] = city.url
+    return cities, False
 
 
 async def _get_response_json_by_(user_input: str, lang_code: str) -> dict:
@@ -43,32 +50,9 @@ async def _get_response_json_by_(user_input: str, lang_code: str) -> dict:
             return await resp.json()
 
 
-def _is_match_100_in_(cities: List[City], user_input: str) -> bool:
-    """Checks if the data items match 100% with the user input."""
-    is_match_100 = cities and (
-        len(cities) == 1 or cities[0].name.lower() == user_input
-    )
-    if is_match_100:
-        return _is_not_terrorist_(cities[0])
-    return False
-
-
-def _is_not_terrorist_(city: City) -> bool:
+def _is_terrorist_(city: City) -> bool:
     """Checks if the data item is terrorist country."""
-    return city.country not in ("Росія", "Россия", "Russia")
-
-
-def _get_filtered_(cities: List[List[str]], lang_code: str) -> OrderedDict:
-    """Returns filtered ordered dict of cities."""
-    filtered_cities = OrderedDict()
-
-    for city in cities:
-        if _is_not_terrorist_(city):
-            country_flag_emoji = _get_country_flag_emoji_by_(
-                city.country, lang_code
-            )
-            filtered_cities[f"{country_flag_emoji} {city.name}"] = city.url
-    return filtered_cities
+    return city.country in ("Росія", "Россия", "Russia")
 
 
 def _get_country_flag_emoji_by_(country_name: str, lang_code: str) -> str:
