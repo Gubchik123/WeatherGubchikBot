92c62abe1cd832057730e70209c87b6c4b686391
Nikita <nikita.gubarev31@gmail.com>
2024-05-08 13:59:39 +0200

    Update the help command response based on chat type

--- bot/handlers/users/commands/help.py
+++ bot/handlers/users/commands/help.py
@@ -13,26 +13,44 @@ router = Router()
 @before_handler_clear_state
 async def handle_help_command(message: Message, *args):
     """Handles the /help command."""
-    await message.answer(
-        _(
-            "Bot commands:\n"
-            "/start - Start working with the bot\n"
-            "/help - Get basic usage rules\n"
-            "/menu - Get main menu\n"
-            "/weather - Get weather forecast\n"
-            "/moon - Get moon phase\n"
-            "/profile - Get user profile\n"
-            "/mailing - Get mailing menu / Sign up for the newsletter\n"
-            "/goodbye - Pause working with the bot\n\n"
-            "Sign up for the newsletter (/mailing) to receive daily weather information in the city of your choice (you can turn it off at any time)\n\n"
-            "I advise you to use the buttons or commands for the intended result\n\n"
-            "Enjoy using!!!\n\n"
-            "Bot author contacts:\n"
-            "CV site: https://hubariev.com\n"
-            "LinkedIn: https://www.linkedin.com/in/nikita-hubariev\n"
-            "Instagram: https://www.instagram.com/notwhale.1746\n\n"
-            "Other projects of the author are available on:\n"
-            "Project board: https://portfolio.hubariev.com\n"
-            "GitHub: https://github.com/Gubchik123\n"
+    if message.chat.type == "private":
+        await message.answer(
+            _(
+                "Bot commands:\n"
+                "/start - Start working with the bot\n"
+                "/help - Get basic usage rules\n"
+                "/menu - Get main menu\n"
+                "/weather - Get weather forecast\n"
+                "/moon - Get moon phase\n"
+                "/profile - Get user profile\n"
+                "/mailing - Get mailing menu / Sign up for the newsletter\n"
+                "/goodbye - Pause working with the bot\n\n"
+                "Sign up for the newsletter (/mailing) to receive daily weather information in the city of your choice (you can turn it off at any time)\n\n"
+                "I advise you to use the buttons or commands for the intended result\n\n"
+                "Enjoy using!!!\n\n"
+                "Bot author contacts:\n"
+                "CV site: https://hubariev.com\n"
+                "LinkedIn: https://www.linkedin.com/in/nikita-hubariev\n"
+                "Instagram: https://www.instagram.com/notwhale.1746\n\n"
+                "Other projects of the author are available on:\n"
+                "Project board: https://portfolio.hubariev.com\n"
+                "GitHub: https://github.com/Gubchik123\n"
+            )
         )
-    )
+    else:
+        await message.answer(
+            _(
+                "Bot commands:\n"
+                "/help - Get basic usage rules\n"
+                "/moon - Get moon phase\n"
+                "/weather - Get weather forecast\n\n"
+                "Enjoy using!!!\n\n"
+                "Bot author contacts:\n"
+                "CV site: https://hubariev.com\n"
+                "LinkedIn: https://www.linkedin.com/in/nikita-hubariev\n"
+                "Instagram: https://www.instagram.com/notwhale.1746\n\n"
+                "Other projects of the author are available on:\n"
+                "Project board: https://portfolio.hubariev.com\n"
+                "GitHub: https://github.com/Gubchik123\n"
+            )
+        )
